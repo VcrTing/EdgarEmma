52c753651ede207a172665fef4f4b0e8bc99e724
vcrting@163.com <vcrting@163.com>
2022-03-31 19:02:40 +0800

    000

--- src/components/plant/ToolReciveWordpress.vue
+++ src/components/plant/ToolReciveWordpress.vue
@@ -3,18 +3,48 @@
     export default {
         methods: {
             returnMessage(event){
-                console.log('接收 Wordpress 的数据：')
+                console.log('接收 Wordpress 的数据：Event =', event, ' TEST =', this.conf.TEST)
                 let data = event.data
                 console.log('data =', data)
-                this.$emit('recive', data)
+                console.log('开始登录')
+                this.reciveData(data)
             },
+
+            // 
+            async doLogin(plant) {
+                let res = await this.serv.user.user_from_strapi(this, plant.wordpress_id)
+                console.log('登录的用户 =', res, plant)
+                if (res) { await this.$store.commit('change', [ 'user', res ]);
+                console.log('结束登录') }
+            },
+
+            async ser_plant(plant) {
+                let tk = plant.token ? plant.token : [ ]
+                tk = tk.length > 0 ? tk[ 0 ] : null
+                tk = { wordpress_id: plant.user_id, token: tk, is_admin: plant.is_admin }
+                await this.$store.commit('change', [ 'plant', tk ])
+                console.log('序列化后的数据 tk =', tk)
+                return tk
+            },
+
+            async reciveData(data) {
+                console.log('接受的数据 recive =', data)
+                let res = data && data.params ? data.params : null
+                console.log('接受的数据 res =', res)
+                res = res ? this.ser_plant(res) : null
+                if (!this.conf.TEST) {
+                    console.log('储存 TOKEN =', tk)
+                    await this.$store.commit('change', [ 'token', tk ]) 
+                }; this.doLogin(res)
+            }
         },
         created() {
             console.log('已加载 message 方法。')
             window.addEventListener("message", this.returnMessage);
         },
         destroyed() {
-            window.removeEventListener('message', this.returnMessage);
+            console.log('销毁 message 方法。')
+            // window.removeEventListener('message', this.returnMessage);
         }
     }
 </script>
