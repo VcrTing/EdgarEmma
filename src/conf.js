5481da268c2e5acf92701486e664c459ac8abbec
VcrTing <vcrting@163.com>
2022-06-20 14:33:03 +0800

    0

--- src/conf.js
+++ src/conf.js
@@ -6,8 +6,8 @@ const baseURL = 'https://strapi07.svr.up5d.com'
 
 const API = baseURL // + '/api'
 
-const VERSION = '0.8'
-const VERSION_TIMED = '2022-04-12'
+const VERSION = '0.85'
+const VERSION_TIMED = '2022-06-20'
 
 const ENDPOINT = {
   user: '/users',
