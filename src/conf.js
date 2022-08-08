e219daf27ad387f5ca08bad6e6ecf329f606384a
VcrTing <vcrting@163.com>
2022-08-08 22:29:02 +0800

    0

--- src/conf.js
+++ src/conf.js
@@ -1,12 +1,12 @@
 
-const TEST = true
+const TEST = false
 
-// const baseURL = 'https://strapi07.svr.up5d.com'
-const baseURL = 'http://127.0.0.1:1337'
+const baseURL = 'https://strapi07.svr.up5d.com'
+// const baseURL = 'http://127.0.0.1:1337'
 
 const API = baseURL // + '/api'
 
-const VERSION = '1.02'
+const VERSION = '1.03'
 const VERSION_TIMED = '2022-08-08'
 
 const ENDPOINT = {
